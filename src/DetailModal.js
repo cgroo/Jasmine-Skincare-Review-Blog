c365cc68c3b497fd7e04aa14160344b0fe7f6719
cgroo <glennangelo78@gmail.com>
2026-06-11 16:19:01 +1200

    Added popup for onClick event on reviewcard

--- src/DetailModal.js
+++ src/DetailModal.js
@@ -1,5 +1,26 @@
-export function DetailModal({review, onClose}) {
+import { Stars } from './Stars'
+
+export default function DetailModal({review, onClose}) {
+    if (!review) {
+        return null;
+    }
     return(
-        this
+        <div className = "popupBackground" onClick = {onClose}>
+            <div className = "modalBox" onClick = {e => e.stopPropagation()}>
+                <button className = "closeButton" onClick = {onClose}>
+                    X
+                </button>
+                <div className = "bodyReview">
+                    <p>{review.emoji}</p>
+                    <p>{review.name}</p>
+                    <p>{review.cat}</p>
+                    <p>{review.brand}</p>
+                    <p>{review.review}</p>
+                </div>
+                <div className = "ratingReview">
+                    <Stars productRating={review.rating} />
+                </div>
+            </div>
+        </div>
     )
 }
