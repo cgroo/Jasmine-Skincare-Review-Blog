e61463a985516ec9d8c4300f195fb5200e4bafa4
cgroo <glennangelo78@gmail.com>
2026-06-12 16:17:09 +1200

    start app.js
    created needed state variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+import {CATEGORIES, INITIAL_REVIEWS } from './data';
+import ReviewCard from './ReviewCard';
+import NewReviewModal from './NewReviewModal';
+import DetailModal from './DetailModal';
+
 export default function App() {
+const [reviews, setReviews] = useState(INITIAL_REVIEWS);
+const [filter, setFilters] = useState('All');
+const [query, setQuery] = useState('');
+const [showNew, setShowNew] = useState(false); 
+const [detail, setDetail] = useState(null);
+
+const visible = []
+handlePublish()
+  //add newReview to fron of reviews array
+  // close modal
   return(
-    <div>
-      <h1>Jasmine's Skincare Review Blog!</h1>
-    </div>
+    
   );
 }
